e142a71c3a8bf2a866db9ec09f8d77a935de7df1
danlin <asmob2011@gmail.com>
2024-10-11 22:32:34 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+//外部套件會放上面
+
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import Input from './components/Input';
+import './assets/all.scss';
+
 
 function App() {
+  const [text, setText] =useState('');
+
+  const onChangeHandler = (e) =>{
+    setText(e.target.value)
+  }
+
+  useEffect(()=>{
+    (async()=>{
+    // 取得.env 環境變數 使用 process.env
+    const path = process.env.REACT_APP_PATH
+     const result = await axios.get(path)
+     console.log(result)
+    })()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         <button type="button" className="btn btn-primary">Primary</button>
         </a>
+        <Input id="sampleText" text="這是一個input" value={text} onChangeHandler={onChangeHandler}></Input>
+        {text}
       </header>
     </div>
   );
